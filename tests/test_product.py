a3b5b631f4c12ca3899906061df949bd63e4772c
Kseniya Druzhinina <drksu91@mail.ru>
2025-10-05 22:30:23 +0300

    Исправления

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,15 +1,19 @@
-from src.product import Product
 import pytest
+
+from src.product import Product
+
 
 @pytest.fixture
 def sample_product():
     return Product("Test Product", "Test Description", 100.0, 10)
+
 
 def test_product_initialization(sample_product):
     assert sample_product.name == "Test Product"
     assert sample_product.description == "Test Description"
     assert sample_product.price == 100.0
     assert sample_product.quantity == 10
+
 
 def test_product_attributes():
     product = Product("Phone", "Smartphone", 500.0, 5)
