466328c00a9d16c925a4d8062d8f5eab14aa7869
Kseniya Druzhinina <drksu91@mail.ru>
2025-11-09 15:42:47 +0300

    Homework_15_1

--- src/category.py
+++ src/category.py
@@ -12,6 +12,11 @@ class Category:
         Category.category_count += 1
         Category.product_count += len(products)
 
+    def __str__(self):
+        """Строковое представление категории"""
+        total_quantity = sum(product.quantity for product in self.__products)
+        return f"{self.name}, количество продуктов: {total_quantity} шт."
+
     def add_product(self, product: Product):
         """Добавляет продукт в категорию"""
         self.__products.append(product)
@@ -20,18 +25,31 @@ class Category:
     @property
     def products(self):
         """Геттер для получения списка товаров в формате строк"""
-        products_info = []
-        for product in self.__products:
-            # Форматируем каждый продукт строго по шаблону с \n в конце
-            product_info = (
-                f"{product.name}, {product.price} руб. "
-                f"Остаток: {product.quantity} шт.\n"
-            )
-            products_info.append(product_info)
-        # Объединяем все строки продуктов
-        return "".join(products_info)
+        return "".join(str(product) + "\n" for product in self.__products)
 
     @property
     def products_list(self):
         """Геттер для получения исходного списка объектов продуктов"""
         return self.__products
+
+    def __iter__(self):
+        """Возвращает итератор для продуктов категории"""
+        return CategoryIterator(self.__products)
+
+
+class CategoryIterator:
+    """Итератор для перебора продуктов в категории"""
+
+    def __init__(self, products: list):
+        self.products = products
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index < len(self.products):
+            product = self.products[self.index]
+            self.index += 1
+            return product
+        raise StopIteration
